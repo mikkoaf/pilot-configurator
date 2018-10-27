eee4fbcb06aaeb733627b2feab380f3799767dfe
Aleksi <aleksi_lahtinen@hotmail.com>
2018-10-27 21:17:28 +0300

    my first commit

---
+++ Projektia/createdb.php
@@ -0,0 +1,109 @@
+<?php
+function jal_install() {
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'Users';
+		
+	$table_name2 = $wpdb->prefix . 'Questions';
+	
+	$table_name3 = $wpdb->prefix . 'Company_answer';
+	
+	$table_name4 = $wpdb->prefix . 'School_answer';
+	
+	$charset_collate = $wpdb->get_charset_collate();
+	
+	
+	$sql = "CREATE TABLE $table_name(
+		user_id INT(3) AUTO_INCREMENT,
+		name VARCHAR(30) NOT NULL,
+		status VARCHAR(30) NOT NULL,
+		email VARCHAR(50),
+		PRIMARY KEY (user_id)
+		);
+		CREATE TABLE $table_name2(
+		question_id INT(3) AUTO_INCREMENT,
+		setti VARCHAR(20S) NOT NULL,
+		question VARCHAR(30) NOT NULL,
+		theme VARCHAR(300),
+		comment VARCHAR(300),
+		PRIMARY KEY (question_id)
+		);
+		CREATE TABLE $table_name3(
+		company_id INT(3),
+		user_id int(3) NOT NULL,
+		question_id INT(3) NOT NULL,
+		answer_max INT(1),
+		answer_min INT(1),
+		answer_val INT(1),
+		comment VARCHAR(300),
+		FOREIGN KEY (user_id) REFERENCES $table_name (user_id),
+		FOREIGN KEY (question_id) REFERENCES $table_name2 (question_id),
+		PRIMARY KEY (question_id)
+		);
+		CREATE TABLE $table_name4(
+		school_id INT(3) NOT NULL,
+		user_id INT(3) NOT NULL,
+		question_id INT(3) NOT NULL,
+		answer INT(1),
+		comment VARCHAR(300),
+		FOREIGN KEY (user_id) REFERENCES $table_name (user_id),
+		FOREIGN KEY (question_id) REFERENCES $table_name2 (question_id),
+		PRIMARY KEY (question_id)
+		)$charset_collate;";
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+}
+function jal_install_data() {
+	global $wpdb;
+	
+	$name = 'Herwoodin koulu';
+	$status = 'Koulu';
+	$email = 'Herwoodinkoulu@herwoord.com';
+	
+	
+	$table_name = $wpdb->prefix . 'Users';
+	
+	$wpdb->insert( 
+		$table_name, 
+		array( 
+			'name' => $name, 
+			'status' => $status,
+			'email' => $email,
+		) 
+	);
+    $table_name2 = $wpdb->prefix . 'Questions';
+
+    $name1 = 'Kauppisen maansiirto Oy';
+	$status1 = 'Yritys';
+	$email1 = 'kauppisen.maansiirto@tyoukko.com';
+    
+    $wpdb->insert( 
+		$table_name, 
+		array( 
+			'name' => $name1, 
+			'status' => $status1,
+			'email' => $email1,
+		) 
+	);
+	$table_name2 = $wpdb->prefix . 'Questions';
+	
+	$settt = 'Yksityisia';
+	$questii = 'Mika maa, mika päiva?';
+	$dunno = 'Teema?';
+	$wpdb->insert( 
+		$table_name2, 
+		array( 
+			'setti' => $settt,
+			'question' => $questii,
+			'theme' => $dunno
+		)
+	);
+	$wpdb->insert( 
+		'wp_Questions', 
+		array( 
+			'setti' => 'Yksityisia',
+			'question' => 'Mis on mailman punaisimmat?',
+			'theme' => 'Teema?'
+		)
+	);
+}
+?>
---
+++ Projektia/pilot-configurator.php
@@ -0,0 +1,46 @@
+<?php
+/**
+Plugin Name: Pilot Configurator
+Plugin URI:
+Description: Pilot Configurator for providing matchmaking in the Inno-Oppiva project
+Author: Mikko Ala-Fossi
+Version: 0.0.1
+Text Domain: pilot-configurator
+-----------------
+Glossary:
+User - a WordPress user account
+*/
+#namespace Inno_Oppiva;
+// Deny direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+  die( ' Access denied!' );
+}
+/**
+  * Define plugin settings as PHP globals.
+  */
+define( 'PILOT_CONFIGURATOR_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PILOT_CONFIGURATOR_PLUGIN_VERSION', '0.0.1' );
+/**
+  * Require plugin files.
+  *
+  */
+require PILOT_CONFIGURATOR_DIR_PATH . 'includes/class-pilot-configurator.php';
+require PILOT_CONFIGURATOR_DIR_PATH . 'includes/admin/class-pilot-configurator-admin.php';
+require_once (dirname(__FILE__) . '/includes/createdatabase/createdbtest.php');
+require_once (dirname(__FILE__) . '/includes/questionaire-creation.php');
+register_activation_hook (__FILE__, 'jal_install');
+register_activation_hook (__FILE__, 'jal_install_data');
+register_activation_hook (__FILE__, 'createquestinaires');
+/**
+  * Begins the execution of the plugin. Pilot_Configurator_Admin is required only when
+  * currently in admin, but Pilot_Configurator needs to be initialized anyway.
+  */
+function pilot_configurator_init() {
+  $pilot_configurator_client = new Inno_Oppiva\Pilot_Configurator_Client();
+  $pilot_configurator_client->init();
+  if ( is_admin() ) {
+    $pilot_configurator_admin = new Inno_Oppiva\Admin\Pilot_Configurator_Admin();
+    $pilot_configurator_admin->init();
+  }
+}
+pilot_configurator_init();
