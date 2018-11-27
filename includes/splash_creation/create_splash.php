3d1e9ca13131b7c82737b593a79f3071713c5e7d
Unknown <nuutti.heinasmaki@hotmail.fi>
2018-11-27 11:19:36 +0200

    Add remove_splashes and rewrite create_splashes for simplicity

--- includes/splash_creation/create_splash.php
+++ includes/splash_creation/create_splash.php
@@ -1,55 +1,64 @@
 <?php
-function create_splashes($results) {
+function create_splashes(){
 	global $wpdb;
 	
 	$author_id = 1;
-	$slug = 'schoolsplash';
 	$title = 'Tervetuloa';
+
+	$slug = 'school_splash';
+	$link = 'TODO';
+	
 	ob_start();
-		echo '<p>Tervetuloa Inno-oppiva järjestelmään</p><p>Jos käytät järjestelmää ensimmäistä kertaa, toivomme että vastaisitte kyselylomakkeisiin alla olevasta linkistä:</p><a href="">Kyselylomakkeisiin</a>';
+		echo '<p>Tervetuloa Inno-oppiva järjestelmään</p><p>Jos käytät järjestelmää ensimmäistä kertaa, toivomme että vastaisitte kyselylomakkeisiin alla olevasta linkistä:</p><a href="'.$link.'">Kyselylomakkeisiin</a>';
 	$content = ob_get_contents(); 
 	ob_end_clean();
 	
-	if( null == get_page_by_path( $title, 'OBJECT', 'page') ) {
-
-		$post_id = wp_insert_post(
-			array(
-				'comment_status'	=>	'closed',
-				'ping_status'		=>	'closed',
-				'post_author'		=>	$author_id,
-				'post_name'			=>	$slug,
-				'post_title'		=>	$title,
-				'post_content'		=>	$content,
-				'post_status'		=>	'publish',
-				'post_type'			=>	'page'
-			)
-		);
-	}
+	wp_insert_post(
+		array(
+			'comment_status'	=>	'closed',
+			'ping_status'		=>	'closed',
+			'post_author'		=>	$author_id,
+			'post_name'			=>	$slug,
+			'post_title'		=>	$title,
+			'post_content'		=>	$content,
+			'post_status'		=>	'publish',
+			'post_type'			=>	'page'
+		)
+	);
 	
-		
-	$author_id = 1;
-	$slug = 'companysplash';
-	$title = 'Tervetuloa';
+	$slug = 'company_splash';
+	$link = 'TODO';
+
 	ob_start();
-		echo '<p>Tervetuloa Inno-oppiva järjestelmään</p><p>Jos käytät järjestelmää ensimmäistä kertaa, toivomme että vastaisitte kyselylomakkeisiin alla olevasta linkistä:</p><a href="">Kyselylomakkeisiin</a>';
+		echo '<p>Tervetuloa Inno-oppiva järjestelmään</p><p>Jos käytät järjestelmää ensimmäistä kertaa, toivomme että vastaisitte kyselylomakkeisiin alla olevasta linkistä:</p><a href="'.$link.'">Kyselylomakkeisiin</a>';
 	$content = ob_get_contents(); 
 	ob_end_clean();
 	
-	if( null == get_page_by_path( $title, 'OBJECT', 'page') ) {
+	wp_insert_post(
+		array(
+			'comment_status'	=>	'closed',
+			'ping_status'		=>	'closed',
+			'post_author'		=>	$author_id,
+			'post_name'			=>	$slug,
+			'post_title'		=>	$title,
+			'post_content'		=>	$content,
+			'post_status'		=>	'publish',
+			'post_type'			=>	'page'
+		)
+	);
+}
 
-		$post_id = wp_insert_post(
-			array(
-				'comment_status'	=>	'closed',
-				'ping_status'		=>	'closed',
-				'post_author'		=>	$author_id,
-				'post_name'			=>	$slug,
-				'post_title'		=>	$title,
-				'post_content'		=>	$content,
-				'post_status'		=>	'publish',
-				'post_type'			=>	'page'
-			)
-		);
-	}	
+function remove_splashes(){
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'posts';
+
+	$slug = 'school_splash';
+	$result = $wpdb->get_row("SELECT ID FROM " . $table_name . " WHERE post_name='" . $slug . "'", 'ARRAY_N');
+	wp_delete_post($result[0], true);
+	
+	$slug = 'company_splash';
+	$result = $wpdb->get_row("SELECT ID FROM " . $table_name . " WHERE post_name='" . $slug . "'", 'ARRAY_N');
+	wp_delete_post($result[0], true);
 }
 
 ?>
