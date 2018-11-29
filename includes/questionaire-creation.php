53933b3d69c14f2c34e54f24172e961ea6e23d95
Jani Aakio <Aakio.Jani.P@student.uta.fi>
2018-11-29 14:27:16 +0000

    Company SQL tallennus

--- includes/questionaire-creation.php
+++ includes/questionaire-creation.php
@@ -3,24 +3,36 @@ function createquestinaires()	{
 	global $wpdb;
 	require_once (dirname(__FILE__) . '/createpost/createpost.php');
 	$settisets = $wpdb->get_results("SELECT DISTINCT question_set FROM wp_Questions");
+	$amount = $wpdb->num_rows;
 	$x = 1;
 	foreach($settisets as $setti){
 		$name = $setti->question_set;
 		$results = $wpdb->get_results("SELECT * FROM wp_Questions WHERE question_set = '".$name."';");
-		create_schoolquestions($results, $x);
-		create_companyquestions($results, $x);
+		create_schoolquestions($results, $x, $amount);
+		create_companyquestions($results, $x, $amount);
 		$x = $x + 1;
 	}
 }
 
 function test_echo(){
-	
+	global $wpdb;
 	
 	parse_str($_POST['cont'], $newarray);
-	print_r($newarray);
-	//foreach($data as $key => $value) {
-	//echo $key . "<- : ->" . $value;
-		//}
+	$uid = get_current_user_id();
+	
+	foreach($newarray as $key => $val){
+		$namenumber = explode("_", $key);
+
+		if($namenumber[0] == "cqans"){
+			$minmax = explode(",", $val);
+		}
+		else{
+			$wpdb->query(
+		  "INSERT INTO wp_Company_answer VALUES 
+		  (NULL, $uid, $namenumber[1], $minmax[1], $minmax[0], $val)
+		  ");
+		}
+	}
 	die();
 }
 ?>
