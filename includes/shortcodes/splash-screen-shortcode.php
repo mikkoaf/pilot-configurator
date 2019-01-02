5bc0a84dd739c6c538b4cc469a5c20f5cc9009f9
Nuutti <nuutti.heinasmaki@hotmail.fi>
2019-01-02 16:07:37 +0200

    Update splash page to handle unlogged users and add links

--- includes/shortcodes/splash-screen-shortcode.php
+++ includes/shortcodes/splash-screen-shortcode.php
@@ -7,21 +7,40 @@ if ( ! defined( 'ABSPATH' ) ) {
   error_log('Shortcodes added');
 
 function pilotcfg_any_splash( $atts = [], $content = null, $tag = 'pilotcfg_any_splash' ) {
+  if ( !is_user_logged_in() ){
+    wp_redirect( wp_login_url() );
+  }
+
   ob_start();
   if ( ( !current_user_can( 'company') && !current_user_can('school') || current_user_can( 'administrator') ) ){
-    echo '<p>Tervetuloa Inno-oppiva järjestelmään</p>
+     echo '<p>Tervetuloa Inno-oppiva järjestelmään</p>
       <p>Sinulle ei vielä ole annettu järjestelmän omaa roolia, joten et voi käyttää kaikkia palvelun toimintoja</p>
       <p>Odottaessasi ylläpidon toimia, voisit kertoa profiilissa itsestäsi</p>
       <p>Palvelu tulee hyödyntämään kyseistä tietoa näyttäessäsi organisaatiotanne</p>';
   }
 
-  if ( current_user_can( 'company') || current_user_can( 'administrator') ){
-    $link = home_url( '/company_question1/', 'https' );
-    echo '<p>Tervetuloa Inno-oppiva järjestelmään</p><p>Jos käytät järjestelmää ensimmäistä kertaa, toivomme että vastaisitte kyselylomakkeisiin alla olevasta linkistä:</p><a href="' . $link . '">Kyselylomakkeisiin</a>';
+  if ( current_user_can( 'administrator') ){
+     echo '<br>';
   }
 
+  if ( current_user_can( 'company') || current_user_can( 'administrator') ){
+    $link = home_url( '/company_question1/', 'https' );
+    $results_link = home_url( '/view_results/', 'https' );
+    echo '<p>Tervetuloa Inno-oppiva järjestelmään</p><p>Jos käytät järjestelmää ensimmäistä kertaa, toivomme että vastaisitte kyselylomakkeisiin alla olevasta linkistä:</p>
+         <a href="' . $link . '">Kyselylomakkeisiin</a>
+         <br><p><a href="' . $results_link . '">Matchmaking tulosten katsaus</a></p>';
+  }
+
+  if ( current_user_can( 'administrator') ){
+   echo '<br>';
+}
+
   if ( current_user_can( 'school') || current_user_can( 'administrator') ){
-      echo '<p>Tervetuloa Inno-oppiva järjestelmään</p><p>Alle on listattuna tuotteet joiden kyselyihin voitte vastata:</p><span id="company_list">Tähän pitäisi tulla vastanneet userit</span>';
+    $results_link = home_url( '/view_results/', 'https' );
+    echo '<p>Tervetuloa Inno-oppiva järjestelmään</p>
+         <p>Alle on listattuna tuotteet joiden kyselyihin voitte vastata:</p>
+         <span id="company_list">Tähän pitäisi tulla vastanneet userit</span>
+         <br><p><a href="' . $results_link . '">Matchmaking tulosten katsaus</a></p>';
   }
   return ob_get_clean();
 }
